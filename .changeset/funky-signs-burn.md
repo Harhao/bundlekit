0e06ecfc3eae39f4c96395a49499f4ff1855acbc
harhao <lvhuahao@corp.netease.com>
2026-05-28 14:23:03 +0800

    fix: 修改模版lib模式问题

---
+++ .changeset/funky-signs-burn.md
@@ -0,0 +1,8 @@
+---
+"@bundlekit/plugin-react": patch
+"@bundlekit/plugin-mock": patch
+"@bundlekit/plugin-vue": patch
+"@bundlekit/cli": patch
+---
+
+修复项目的 lib 模式问题
--- packages/bundlekit-plugin-mock/generator/index.ts
+++ packages/bundlekit-plugin-mock/generator/index.ts
@@ -1,5 +1,6 @@
 import { addPluginToConfig } from "@bundlekit/shared-utils";
+import type { IGeneratorAPI } from "@bundlekit/shared-utils";
 
-export default async function generate(context: string): Promise<void> {
+export default async function generate(context: string, api: IGeneratorAPI): Promise<void> {
     addPluginToConfig(context, "@bundlekit/plugin-mock");
 }
